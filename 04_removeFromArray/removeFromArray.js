d89da2fbd4c5b12b81f8a738a62032c710790de6
HarryAhnHS <hongsuhahn@gmail.com>
2023-10-27 18:14:08 +0800

    Completed exercises 1-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    const outArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!args.includes(arr[i])) outArray.push(arr[i]);
+    }  
+    return outArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
 
+    if (start < 0 || end < 0) return "ERROR";
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    let sum = 0;
+    let max = Math.max(start,end);
+    let min = Math.min(start,end);
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
