7e4ee4b281a62de431dba37a096da0e058d53b35
HarryAhnHS <hongsuhahn@gmail.com>
2023-11-11 23:09:42 +0800

    completed 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,35 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce( (total, num) => {
+    return total += num;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce( (prod, num) => {
+    return prod *= num;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return (Math.pow(a,b));
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let fact = 1;
+  for (let i = 1; i<=n; i++) {
+    fact *= i;
+  }
+  return fact;
 };
+
 
 // Do not edit below this line
 module.exports = {
